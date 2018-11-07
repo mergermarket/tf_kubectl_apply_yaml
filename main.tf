f48be4422cbd081fadf65c2ff762237b314b6b12
Keir Badger <keir.badger@acuris.com>
2018-11-07 09:48:25 +0000

    STraw clustch 2

--- main.tf
+++ main.tf
@@ -41,9 +41,9 @@ resource "local_file" "yaml_file" {
 
 resource "null_resource" "apply-yaml" {
   depends_on = ["local_file.kubeconfig", "local_file.yaml_file"]
-  triggers = {
-    yaml_sha1 = "${sha1(file("${path.root}/${var.yaml_filename}"))}"
-  }
+//  triggers = {
+//    yaml_sha1 = "${sha1(file("${path.root}/${var.yaml_filename}"))}"
+//  }
   provisioner "local-exec" {
     command = "kubectl apply -f ${path.root}/${var.yaml_filename}"
   }
